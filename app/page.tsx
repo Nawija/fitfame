cc4565dda593b8525c33c1d2676567df86e639ff
Seovileoo <96625039+Nawija@users.noreply.github.com>
2025-04-03 18:36:00 +0200

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -11,6 +11,7 @@ export default function Home() {
                         fill
                         className="object-cover -z-20"
                         alt="hero"
+                        quality={60}
                     />
                     <div className="absolute inset-0 bg-black/60 -z-20  " />
                     <h1 className="font-bold uppercase text-5xl lg:text-9xl z-10 h-full w-3/4 mx-auto text-center flex items-center justify-center text-white">
