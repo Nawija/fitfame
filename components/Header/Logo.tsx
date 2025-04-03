41cb610de93d4eb05758d2218f5996e2fdf63f83
Seovileoo <96625039+Nawija@users.noreply.github.com>
2025-04-03 18:37:33 +0200

    Update Logo.tsx

--- components/Header/Logo.tsx
+++ components/Header/Logo.tsx
@@ -4,11 +4,13 @@ import Link from "next/link";
 export default function Logo() {
     return (
         <div className="font-bold lg:mr-8 uppercase pb-3 tracking-widest text-2xl lg:text-5xl ">
-            <Link href="/" aria-label="Strona główna" className="relative">
-                <p >FAME</p>
-                <span className="absolute right-0 -bottom-full -translate-y-[60%] text-xl lg:text-3xl">
-                    FIT
-                </span>
+            <Link href="/" aria-label="Strona główna" >
+                <div className="relative">
+                    <p>FAME</p>
+                    <span className="absolute right-0 -bottom-full -translate-y-[60%] text-xl lg:text-3xl">
+                        FIT
+                    </span>
+                </div>
             </Link>
         </div>
     );
