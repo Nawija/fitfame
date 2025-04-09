8b215b8e14725ad2c687724a360c9d66fa11c34d
Valkon <96625039+Nawija@users.noreply.github.com>
2025-04-09 18:37:39 +0200

    Update page.tsx

--- app/(routes)/przepisy/[slug]/page.tsx
+++ app/(routes)/przepisy/[slug]/page.tsx
@@ -2,16 +2,9 @@ import { recipesData } from "@/constants/Przepisy";
 import { notFound } from "next/navigation";
 import { IoTimeOutline } from "react-icons/io5";
 
-export async function generateStaticParams() {
-    return recipesData.map((recipe) => ({
-        slug: recipe.slug,
-    }));
-}
-
-export default function Page({ params }: { params: { slug: string } }) {
-    const { slug } = params;
+export default async function Page({ params }: { params: { slug: string } }) {
+    const { slug } = await params;
     const recipe = recipesData.find((r) => r.slug === slug);
-
     if (!recipe) return notFound();
 
     return (
