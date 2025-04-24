959a985b736933432b22a8ed6b107befe0754d19
Seovileoo <96625039+Nawija@users.noreply.github.com>
2025-04-24 11:03:37 +0200

    Update page.tsx

--- app/(routes)/sklep/page.tsx
+++ app/(routes)/sklep/page.tsx
@@ -1,9 +1,17 @@
-export default function Sklep() {
+import { Recipe } from "@/types/types";
+
+import { RecipeSkeleton } from "@/components/SkeletonUI/RecipeSkeleton";
+import dynamic from "next/dynamic";
+import { getAllProducts } from "@/lib/getProducts";
+
+export const RecipesGrid = ({ recipes }: { recipes: Recipe[] }) => {
     return (
-        <div className="grid grid-cols-4 w-full relative max-w-7xl mx-auto py-12">
-            <div className="bg-red-300 w-full h-full p-4 flex items-center justify-center flex-col">
-                <p>Title</p>
+        <div>
+            <div className="w-full grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-5 gap-3">
+                {getAllProducts.map((produkt, index) => (
+                    <RecipeCard key={index} produkt={recipe} />
+                ))}
             </div>
         </div>
     );
-}
+};
