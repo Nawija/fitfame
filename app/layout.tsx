53de834d92bc391da06da1a3fbcfd8d1cf8ce28e
Valkon <96625039+Nawija@users.noreply.github.com>
2025-04-28 13:22:20 +0200

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,24 +1,23 @@
+import type { Metadata } from "next";
 import "./globals.css";
 import Nav from "@/components/Header/Nav";
 import Footer from "@/components/Footer";
 
-export async function generateMetadata() {
-    return {
-        title: "GRAMFIT",
-        description: `Sprawdź przepis na!`,
-        openGraph: {
-            title: "GRAMFIT",
-            description: `Sprawdź przepis na!`,
-            images: "/images/gramfit.jpg",
-        },
-        twitter: {
-            card: "summary_large_image",
-            title: "GRAMFIT",
-            description: "GRAMFIT",
-            images: "/images/gramfit.jpg",
-        },
-    };
-}
+export const metadata: Metadata = {
+    title: "GramFit - Trener Personalny",
+    description:
+        "GramFit to kompleksowa platforma dla osób pragnących poprawić swoją formę fizyczną. Oferujemy indywidualne plany treningowe, które pomagają osiągnąć wymarzoną sylwetkę, a także darmową bazę przepisów, które wspierają zdrowe odżywianie. Nasz sklep internetowy to miejsce, w którym znajdziesz wysokiej jakości produkty wspierające Twoje cele fitnessowe. Z GramFit osiągniesz sukces, zarówno na siłowni, jak i w kuchni",
+    openGraph: {
+        images: [
+            {
+                url: "/images/gramfit.jpg",
+                width: 1200,
+                height: 630,
+                alt: "GramFit - Trener Personalny",
+            },
+        ],
+    },
+};
 
 export default function RootLayout({
     children,
