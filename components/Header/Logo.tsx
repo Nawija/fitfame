47aa65e7965188ae138efb3261f72db994b731ee
Valkon <96625039+Nawija@users.noreply.github.com>
2025-04-28 11:23:47 +0200

    m

--- components/Header/Logo.tsx
+++ components/Header/Logo.tsx
@@ -3,13 +3,10 @@ import Link from "next/link";
 
 export default function Logo() {
     return (
-        <div className="font-bold lg:mr-8 uppercase pb-3 tracking-widest text-xl lg:text-4xl ">
-            <Link href="/" aria-label="Strona główna" >
+        <div className="font-bold lg:mr-8 uppercase tracking-tight text-xl lg:text-4xl ">
+            <Link href="/" aria-label="Strona główna">
                 <div className="relative">
-                    <p>FAME</p>
-                    <span className="absolute right-0.5 -bottom-full -translate-y-[70%] text-base lg:text-2xl tracking-wide">
-                        FIT
-                    </span>
+                    <p>GRAMFIT</p>
                 </div>
             </Link>
         </div>
